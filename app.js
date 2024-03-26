2dd7636197e565857409b29eb8e231442923aa4d
Whoami <nurlanguliev33@gmail.com>
2024-03-26 13:22:01 -0700

    Java

---
+++ app.js
@@ -0,0 +1,33 @@
+// 1
+// let username = prompt (`Ad daxil edin:`);
+// let Ad = username.length;
+// console.log(Ad + " " + "simvol var");
+
+// 2
+// let Data = (`Bir soz yaz`);
+// let char = prompt (`Birinci herfi yazin`);
+// let Bir = Data.includes(`char`);
+// console.log(`Bir` ? "simvol var" : "simvol yox");
+
+// 3
+// let Data = prompt (`Daxil et`);
+// let char = prompt (`Simvol`);
+// let Bir = Data.charAt(0-1);
+// console.log(`Bir` ? `Başlayır` : `Başlamır`);
+
+
+// 4
+// let Data = prompt (`Daxil et`);
+// let char = prompt (`Simvol`);    
+// let last = Data.charAt(Data.length - 1)== char
+// console.log(last  ? `bitir` : `bitmir`);
+
+// 5
+// let Data = prompt ("Söz daxil edin:");
+// let main = word.indexOf(word[2]);
+// console.log(main);
+
+// 6
+// let Data = prompt(`text: `);
+// let name = data.toUpperCase() == Data;
+// console.log(`${name}`);
